0e11b15aa740c4861b4e3e2764c1ffca2785a0b6
Vladimír Borek <xborek@fi.muni.cz>
2026-03-16 02:27:28 +0100

    add: Validators

---
+++ BusinessLayer/Validators/Validator.cs
@@ -0,0 +1,32 @@
+﻿namespace BusinessLayer.Validators
+{
+    public static class Validator
+    {
+
+        //public static bool IsHex(string s)
+        //{
+        //    return s.All(c =>
+        //        (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')
+        //    );
+        //}
+
+        //public static bool IsBinary(string s)
+        //{
+        //    return s.All(c => c == '0' || c == '1');
+        //}
+
+        public static bool IsHex(string s)
+        {
+            return !string.IsNullOrEmpty(s) && s.All(c =>
+                (c >= '0' && c <= '9') ||
+                (c >= 'a' && c <= 'f') ||
+                (c >= 'A' && c <= 'F'));
+        }
+
+        public static bool IsBinary(string s)
+        {
+            return !string.IsNullOrEmpty(s) && s.All(c => c == '0' || c == '1');
+        }
+
+    }
+}
