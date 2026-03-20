b830ed98d809d865a82ce7296ce27cbcb36a5d03
Vladimír Borek <xborek@fi.muni.cz>
2026-03-20 16:10:52 +0100

    refactored comand objects

--- PV286-project/Cli/ParsedCommands/EncodeCommandParsed.cs
+++ PV286-project/Cli/ParsedCommands/EncodeCommandParsed.cs
@@ -1,15 +1,16 @@
-﻿//using BusinessLayer.enums;
+﻿using BusinessLayer.enums;
 
-//namespace PV286_project.Cli.Commands
-//{
-//    public class EncodeCommandParsed : ParsedCommand
-//    {
-//        public byte[]? Entropy { get; init; }
-//        public ValueFormat Format { get; init; }
+namespace PV286_project.Cli.Commands
+{
+    public class EncodeCommandParsed : ParsedCommand
+    {
+        public byte[]? Entropy { get; init; }
+        public ValueFormat Format { get; init; }
 
-//        public EncodeCommandParsed(byte[]? entropy, ValueFormat format) : base(CommandType.Encode) { 
-//            this.Entropy = entropy;
-//            this.Format = format;
-//        }
-//    }
-//}
+        public EncodeCommandParsed(byte[]? entropy, ValueFormat format) : base(CommandType.Encode)
+        {
+            this.Entropy = entropy;
+            this.Format = format;
+        }
+    }
+}
