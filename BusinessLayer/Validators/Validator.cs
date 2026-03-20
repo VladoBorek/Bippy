93b3cec994292312ec7c8de4aea2fb5d3101ec10
Vladimír Borek <xborek@fi.muni.cz>
2026-03-20 16:11:37 +0100

    deleted: commented out code in validator

--- BusinessLayer/Validators/Validator.cs
+++ BusinessLayer/Validators/Validator.cs
@@ -2,19 +2,6 @@
 {
     public static class Validator
     {
-
-        //public static bool IsHex(string s)
-        //{
-        //    return s.All(c =>
-        //        (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')
-        //    );
-        //}
-
-        //public static bool IsBinary(string s)
-        //{
-        //    return s.All(c => c == '0' || c == '1');
-        //}
-
         public static bool IsHex(string s)
         {
             return !string.IsNullOrEmpty(s) && s.All(c =>
