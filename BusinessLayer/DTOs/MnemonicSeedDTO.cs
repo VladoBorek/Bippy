6a82b4fdd34af919544b2fecc0c22a1d185a2287
Vladimír Borek <xborek@fi.muni.cz>
2026-03-20 15:50:34 +0100

    added mnemonicSeed Dto toString

--- BusinessLayer/DTOs/MnemonicSeedDTO.cs
+++ BusinessLayer/DTOs/MnemonicSeedDTO.cs
@@ -1,4 +1,5 @@
-﻿using NBitcoin;
+﻿using BusinessLayer.enums;
+using NBitcoin;
 
 namespace BusinessLayer.DTOs
 {
@@ -6,6 +7,22 @@ namespace BusinessLayer.DTOs
     {
         public required Mnemonic Mnemonic { get; set; }
         public required byte[] Seed { get; set; }
-        public required bool IsBinary { get; set; }
+        public required ValueFormat Format { get; set; }
+    
+
+        public override string ToString()
+        {
+            
+            return
+                $"Mnemonic : {Mnemonic}{Environment.NewLine}" +
+                $"Seed     : {BytesToString(Seed, Format)}";
+        }
+
+        private static string BytesToString(byte[] bytes, ValueFormat format)
+        {
+            return format == ValueFormat.Bin
+                ? string.Concat(bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')))
+                : Convert.ToHexString(bytes).ToLowerInvariant();
+        }
     }
 }
