cb8820ed1952b34954776637d7b62bf5af7413e4
Martin Mackovič <xmackov1@fi.muni.cz>
2026-03-21 13:31:46 +0100

    rename MnemonicService to CommandService

---
+++ BusinessLayer/Services/CommandService.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.CLI.Utils.Enums;
+using BusinessLayer.DTOs;
+using BusinessLayer.Services.Interfaces;
+using FluentResults;
+using NBitcoin;
+
+namespace BusinessLayer.Services
+{
+    public class CommandService : ICommandService
+    {
+        private readonly int DefaultEntropySizeBytes = 32;
+
+        public Result<EncodeDTO> Encode(byte[]? entropy, ValueFormat format)
+        {
+            var finalEntropy = entropy ?? RandomUtils.GetBytes(DefaultEntropySizeBytes);
+            var mnemonic = new Mnemonic(Wordlist.English, finalEntropy);
+            var seed = mnemonic.DeriveSeed();
+
+            return Result.Ok(
+                new EncodeDTO
+                {
+                    Mnemonic = mnemonic,
+                    Seed = seed,
+                    Format = format,
+                }
+            );
+        }
+    }
+}
---
+++ BusinessLayer/Services/Interfaces/ICommandService.cs
@@ -0,0 +1,11 @@
+﻿using BusinessLayer.CLI.Utils.Enums;
+using BusinessLayer.DTOs;
+using FluentResults;
+
+namespace BusinessLayer.Services.Interfaces
+{
+    public interface ICommandService
+    {
+        Result<EncodeDTO> Encode(byte[]? entropy, ValueFormat format);
+    }
+}
