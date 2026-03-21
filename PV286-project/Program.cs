2c747b21674f803895b8d06ab128a2882a6b10b9
Fefan <xmlynar3@fi.muni.cz>
2026-03-21 11:27:20 +0100

    Added test to check if SCS is working

--- PV286-project/Program.cs
+++ PV286-project/Program.cs
@@ -2,6 +2,12 @@
 using BusinessLayer.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Cryptography;
+
+void Test()
+{
+    using var md5 = MD5.Create(); // SCS should warn about weak hash
+}
 
 namespace PV286_project
 {
