787e51acdc883e1874c46053f74a5fb044a98ce5
Fefan <xmlynar3@fi.muni.cz>
2026-03-22 16:43:49 +0100

    added dummy test to CI

---
+++ PV286.Project.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using BusinessLayer.CLI.Validators;
+using BusinessLayer.CLI.Utils.Enums;
+using ResultPattern;
+
+namespace PV286.Project.Tests
+{
+    [TestFixture]
+    public class EncodeValidatorTests
+    {
+
+        [Test]
+        public void NoEntropy_FormatNotProvided_IsOk()
+        {
+            var result = EncodeValidator.IsValidEncode(
+                entropy: null,
+                formatProvided: false,
+                format: ValueFormat.Hex  // value ignored when entropy is null
+            );
+
+            Assert.That(result.IsSuccess, Is.True, result.Error);
+        }
+    }
+}
