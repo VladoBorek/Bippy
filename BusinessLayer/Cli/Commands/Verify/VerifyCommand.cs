e04d199126128d85b3f5f49a43fb4b3f6dc3b8c6
Vladimír Borek <xborek@fi.muni.cz>
2026-04-10 19:42:42 +0200

    add verify command

---
+++ BusinessLayer/Cli/Commands/Verify/VerifyCommand.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.CLI.Commands;
+using BusinessLayer.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Cli.Commands.Verify
+{
+    public class VerifyCommand : ICliCommand
+    {
+        public string CommandName => "verify";
+
+        private readonly string phrase;
+        private readonly byte[] seed;
+        private readonly ICommandService commandService;
+
+        public VerifyCommand(string phrase, byte[] seed, ICommandService commandService)
+        {
+            this.phrase = phrase;
+            this.seed = seed;
+            this.commandService = commandService;
+        }
+
+        public bool Handle()
+        {
+            var verifyRes = commandService.Verify(phrase, seed);
+
+            if (verifyRes.IsFailed)
+            {
+                Console.Error.WriteLine(verifyRes.Error);
+                return false;
+            }
+
+            Console.Write(verifyRes.Value.ToString());
+            return true;
+        }
+    }
+}
